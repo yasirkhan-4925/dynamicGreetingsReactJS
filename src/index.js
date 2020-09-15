4126c1641b55ac9791fd3cccc776fd4b498c3109
yasirkhan-4925 <iammuhammadyasirkhan@gmail.com>
2020-09-15 19:40:46 +0500

    my first commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,39 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+let currentHour= new Date();
+
+currentHour=currentHour.getHours();
+
+let greetings;
+const cssGreetings={};
+
+if(currentHour>=12 && currentHour <=16)
+{
+   greetings="Good Afternoon"
+   cssGreetings.color="yellow";
+}
+else if(currentHour>=5 && currentHour<12)
+{
+   greetings="Good Morning"
+   cssGreetings.color="green"
+}
+else{
+   greetings="Good Night"
+   cssGreetings.color="black"
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+
+   
+  <div>
+  
+<h1> Hey ! <span style={cssGreetings}> {greetings} </span></h1>
+</div>
+    </>
+  ,
   document.getElementById('root')
 );
 
